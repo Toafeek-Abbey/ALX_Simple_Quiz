6e5eb9e358108a21bac3bae1bbc182c5f593f5b8
Toafeek-Abbey <101677385+Toafeek-Abbey@users.noreply.github.com>
2025-01-19 14:18:20 +0100

    Create quiz.js

---
+++ quiz.js
@@ -0,0 +1,13 @@
+function checkAnswer() {
+    var correctAnswer = 4
+    document.querySelector('input[name=quiz]:checked')
+    var userAnswer = document.querySelector('input[name=quiz]:checked').value
+    if (userAnswer == correctAnswer) {
+        var feedbackId = document.getElementById("feedback")
+        feedbackId.textContent = "Correct! Well done."
+    }else{
+          var feedbackId = document.getElementById("feedback")
+          feedbackId.textContent = "That's incorrect. Try again!"
+    }
+}
+document.getElementById("submit-answer").addEventListener("click", checkAnswer)

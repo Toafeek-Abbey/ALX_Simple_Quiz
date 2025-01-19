ee11d73abc48fe19684232056ec5b53bd0abfb3f
Toafeek-Abbey <101677385+Toafeek-Abbey@users.noreply.github.com>
2025-01-19 14:26:02 +0100

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -2,12 +2,13 @@ function checkAnswer() {
     var correctAnswer = 4
     document.querySelector('input[name=quiz]:checked')
     var userAnswer = document.querySelector('input[name=quiz]:checked').value
-    if (userAnswer === correctAnswer) {
-        var feedbackId = document.getElementById("feedback")
-        feedbackId.textContent = "Correct! Well done."
-    }else{
+   
+    else{
           var feedbackId = document.getElementById("feedback")
           feedbackId.textContent = "That's incorrect. Try again!"
     }
 }
+ if (userAnswer === correctAnswer) {
+        document.getElementById("feedback").textContent = "Correct! Well done."
+    }
 document.getElementById("submit-answer").addEventListener("click", checkAnswer)
